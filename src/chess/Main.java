636e00d07e50c9fec0ffba0894db9b72d0b5463f
Gabriel <pinokjell@gmail.com>
2026-03-03 15:06:08 +0100

    resolve merge conflicts

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package chess;
 
 import chess.model.Game;
@@ -14,20 +14,3 @@ public class Main {
 	}
 
 }
-=======
-package chess;
-
-import chess.model.Game;
-import chess.ui.ChessFrame;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Game game = new Game();
-		ChessFrame frame = new ChessFrame(game);
-		frame.setVisible(true);
-
-	}
-
-}
->>>>>>> a27aabb54f748658a8062a38db47d727cad65670
