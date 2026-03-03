2f3cab684f5ea8170b087c55b81f3940ac396dc6
SenecaOS <vegetainrain@gmail.com>
2026-03-03 15:07:05 +0100

    Update Main.java

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package chess;
 
 import chess.model.Game;
@@ -14,20 +13,3 @@ public class Main {
 	}
 
 }
-=======
-package chess;
-
-import chess.model.Game;
-import chess.ui.ChessFrame;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Game game = new Game();
-		ChessFrame frame = new ChessFrame(game);
-		frame.setVisible(true);
-
-	}
-
-}
->>>>>>> a27aabb54f748658a8062a38db47d727cad65670
