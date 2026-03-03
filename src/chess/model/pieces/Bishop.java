db4e3d413b827d0681ffbc3210665d7b03dd50ba
Gabriel <pinokjell@gmail.com>
2026-03-03 15:08:37 +0100

    Resolve merge conflicts

--- src/chess/model/pieces/Bishop.java
+++ src/chess/model/pieces/Bishop.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package chess.model.pieces;
 
 import chess.model.Board;
@@ -13,20 +12,4 @@ public class Bishop extends Piece {
 	public void calculateLegalMoves() {
 		calculateDiagonalMoves();
 	}
-=======
-package chess.model.pieces;
-
-import chess.model.Board;
-import chess.model.Game;
-
-public class Bishop extends Piece {
-	public Bishop(char color, Board board, Game game) {
-		super(color, board, game);
-
-	}
-
-	public void calculateLegalMoves() {
-		calculateDiagonalMoves();
-	}
->>>>>>> a27aabb54f748658a8062a38db47d727cad65670
 }
